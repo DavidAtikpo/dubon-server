cf3c0cc727cd36cdd3dbaf216086960d85029fbd
DavidAtikpo <davidatikpo44@gmail.com>
2024-12-02 09:16:54 +0000

    methode de payement

--- Routers/User.js
+++ Routers/User.js
@@ -1,9 +1,6 @@
 import express from 'express'
-// import passport from '../config/passport.js';
 import User from "../Controllers/User.js"
-// import userValidator from '../validators/userValidator.js'
 import authMiddleware from '../middleware/authMiddleware.js';
-// import passport from '../config/passport.js';
 
 const router = express.Router()
 
@@ -14,42 +11,6 @@ router.post('/logout',authMiddleware.authMiddleware,User.logout)
 router.post('/forgot-password',User.forgotPassword)
 router.put('/update/:id',User.updatePassword)
 router.post('/reset-password/:id',User.resetPassword)
-router.get('/info/:id'), User.userInfo
-
-// // Routes Google OAuth
-// router.get('/auth/google', 
-//     passport.authenticate('google', { 
-//         scope: ['profile', 'email'],
-//         session: true 
-//     })
-// );
-
-// router.get('/auth/google/callback', 
-//     passport.authenticate('google', { 
-//         failureRedirect: '/login',
-//         session: true
-//     }),
-//     (req, res) => {
-//         res.redirect('/dashboard');
-//     }
-// );
-
-// // Routes Facebook OAuth
-// router.get('/auth/facebook', 
-//     passport.authenticate('facebook', { 
-//         scope: ['email'],
-//         session: true 
-//     })
-// );
-
-// router.get('/auth/facebook/callback', 
-//     passport.authenticate('facebook', { 
-//         failureRedirect: '/login',
-//         session: true
-//     }),
-//     (req, res) => {
-//         res.redirect('/dashboard');
-//     }
-// );
+router.get('/info/:id', User.userInfo)
 
 export default router
