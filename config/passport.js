dafb50d8da2f9f9e24292becfecaaf1a24f4d573
DavidAtikpo <davidatikpo44@gmail.com>
2024-12-02 00:48:51 +0000

    Fresh start without sensitive data

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,7 @@ import passport from 'passport';
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/api/auth/google/callback"
+    callbackURL: "/auth/google/callback"
 }, async (accessToken, refreshToken, profile, done) => {
     const email = profile.emails[0].value;
     const name = profile.displayName;
