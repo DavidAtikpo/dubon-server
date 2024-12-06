75e8f3c237b6e93d629c70d6110927e93b4d6774
DavidAtikpo <davidatikpo44@gmail.com>
2024-12-06 00:18:59 +0000

    api pour le render

--- config/refreshToken.js
+++ config/refreshToken.js
@@ -1,12 +1,17 @@
+
 import jwt from "jsonwebtoken";
 
 const generateRefreshToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
+  // Vérifiez que la clé est bien chargée
+  const secret = process.env.JWT_SECRET;
+  if (!secret) {
+    throw new Error("JWT_SECRET must be defined in the environment variables");
+  }
+
+  // Créez le token avec la clé secrète
+  return jwt.sign({ id }, secret, {
     expiresIn: "1h", // Token expirera après 1 heure
   });
 };
 
-
 export default generateRefreshToken;
-
-
