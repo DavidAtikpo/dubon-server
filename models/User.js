dd2a2d7f3cbbffdcc79a6538fa618c44d079871f
DavidAtikpo <davidatikpo44@gmail.com>
2024-12-16 17:01:19 +0000

    production

--- models/User.js
+++ models/User.js
@@ -1,74 +1,53 @@
 import { DataTypes } from 'sequelize';
+import { sequelize } from '../config/dbConfig.js';
 
-export default (sequelize) => {
-  const User = sequelize.define('User', {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true
-      }
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    role: {
-      type: DataTypes.ENUM('user', 'seller', 'admin', 'superAdmin'),
-      defaultValue: 'user'
-    },
-    is_trial_active: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    },
-    trial_end_date: {
-      type: DataTypes.DATE,
-      allowNull: true
-    },
-    subscription_paid: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    },
-    is_blocked: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    },
-    profilePhotoURL: {
-      type: DataTypes.STRING,
-      defaultValue: '/uploads/default-user-profile-svgrepo-com (1).svg'
-    },
-    email_verified: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    },
-    email_verification_token: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    email_verification_expires: {
-      type: DataTypes.DATE,
-      allowNull: true
-    },
-    refresh_token: {
-      type: DataTypes.STRING,
-      allowNull: true
-    }
-  }, {
-    timestamps: true,
-    underscored: true,
-    tableName: 'users',
-    freezeTableName: true
-  });
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  role: {
+    type: DataTypes.ENUM('user', 'admin', 'seller'),
+    defaultValue: 'user'
+  },
+  profilePhotoUrl: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  emailVerified: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  },
+  emailVerificationToken: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  emailVerificationExpires: {
+    type: DataTypes.DATE,
+    allowNull: true
+  },
+  refreshToken: {
+    type: DataTypes.STRING,
+    allowNull: true
+  }
+}, {
+  timestamps: true,
+  underscored: true,
+  tableName: 'users'
+});
 
-  return User;
-};
+export default User;
